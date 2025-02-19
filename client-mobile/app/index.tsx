348470a5eeb2f53f3936426a1618b299f3619b2d
Adam Mehdaoui <adam.mehdaouijorge@gmail.com>
2025-02-19 22:17:39 +0100

    chore: custom text

--- client-mobile/app/index.tsx
+++ client-mobile/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Welcome on lootopia, your AR experience</Text>
     </View>
   );
 }
