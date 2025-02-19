7957d437749d4e231c46da152ff4bd05aea0b368
Adam Mehdaoui <adam.mehdaouijorge@gmail.com>
2025-02-19 22:47:17 +0100

    WIP: code for AR (TODO: use expo prebuilt not expo go)

--- client-mobile/app/index.tsx
+++ client-mobile/app/index.tsx
@@ -1,15 +1,50 @@
-import { Text, View } from "react-native";
+import { ViroARScene, ViroText, ViroTrackingReason, ViroTrackingStateConstants } from "@reactvision/react-viro";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+// const HelloWorldSceneAR = () => {
+//   const [text, setText] = useState("Initializing AR...");
+
+//   function onInitialized(state: any, reason: ViroTrackingReason) {
+//     console.log("onInitialized", state, reason);
+//     if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
+//       setText("Hello World!");
+//     } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
+//       // Handle loss of tracking
+//     }
+//   }
+
+//   return (
+//     <ViroARScene onTrackingUpdated={onInitialized}>
+//       <ViroText
+//         text={text}
+//         scale={[0.5, 0.5, 0.5]}
+//         position={[0, 0, -1]}
+//         style={styles.helloWorldTextStyle}
+//       />
+//     </ViroARScene>
+//   );
+// };
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome on lootopia, your AR experience</Text>
+    <View style={styles.view}>
+      {/* <ViroARSceneNavigator
+        autofocus={true}
+        initialScene={{
+          scene: HelloWorldSceneAR,
+        }}
+        style={styles.f1}
+      /> */}
+      <Text>Lootopia</Text>
     </View>
   );
-}
+};
+
+var styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
