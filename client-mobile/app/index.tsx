681a641ea80255fc870f52893f7f2287d5294775
Adam Mehdaoui <adam.mehdaouijorge@gmail.com>
2025-02-23 20:45:24 +0100

    feat: working ar with viro react

--- client-mobile/app/index.tsx
+++ client-mobile/app/index.tsx
@@ -1,50 +1,56 @@
-import { ViroARScene, ViroText, ViroTrackingReason, ViroTrackingStateConstants } from "@reactvision/react-viro";
+import {
+  ViroARScene,
+  ViroARSceneNavigator,
+  ViroText,
+  ViroTrackingReason,
+  ViroTrackingStateConstants,
+} from "@reactvision/react-viro";
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 
-// const HelloWorldSceneAR = () => {
-//   const [text, setText] = useState("Initializing AR...");
+const HelloWorldSceneAR = () => {
+  const [text, setText] = useState("Initializing AR...");
 
-//   function onInitialized(state: any, reason: ViroTrackingReason) {
-//     console.log("onInitialized", state, reason);
-//     if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
-//       setText("Hello World!");
-//     } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
-//       // Handle loss of tracking
-//     }
-//   }
+  function onInitialized(state: any, reason: ViroTrackingReason) {
+    console.log("onInitialized", state, reason);
+    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
+      setText("Hello World!");
+    } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
+      // Handle loss of tracking
+    }
+  }
 
-//   return (
-//     <ViroARScene onTrackingUpdated={onInitialized}>
-//       <ViroText
-//         text={text}
-//         scale={[0.5, 0.5, 0.5]}
-//         position={[0, 0, -1]}
-//         style={styles.helloWorldTextStyle}
-//       />
-//     </ViroARScene>
-//   );
-// };
-
-export default function Index() {
   return (
-    <View style={styles.view}>
-      {/* <ViroARSceneNavigator
-        autofocus={true}
-        initialScene={{
-          scene: HelloWorldSceneAR,
-        }}
-        style={styles.f1}
-      /> */}
-      <Text>Lootopia</Text>
-    </View>
+    <ViroARScene onTrackingUpdated={onInitialized}>
+      <ViroText
+        text={text}
+        scale={[0.5, 0.5, 0.5]}
+        position={[0, 0, -1]}
+        style={styles.helloWorldTextStyle}
+      />
+    </ViroARScene>
+  );
+};
+
+export default () => {
+  return (
+    <ViroARSceneNavigator
+      autofocus={true}
+      initialScene={{
+        scene: HelloWorldSceneAR,
+      }}
+      style={styles.f1}
+    />
   );
 };
 
 var styles = StyleSheet.create({
-  view: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  f1: { flex: 1 },
+  helloWorldTextStyle: {
+    fontFamily: "Arial",
+    fontSize: 30,
+    color: "#ffffff",
+    textAlignVertical: "center",
+    textAlign: "center",
   },
 });
