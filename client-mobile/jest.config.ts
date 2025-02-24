26ec1cd093b2bdc5dbbdb7048242006e25a68ee4
Adam Mehdaoui <90783059+adammehdaoui@users.noreply.github.com>
2025-02-24 20:05:09 +0100

    LOO104: Setup & CI/CD frontend (mobile) - Expo - React Native
    * chore: setup mobile front
    
    * chore: update mobile project + ci + jest test config
    
    * chore: add ci
    
    * fix: ci working directory
    
    * chore: update deps for compatibility
    
    * chore: custom text
    
    * WIP: code for AR (TODO: use expo prebuilt not expo go)
    
    * feat: working ar with viro react
    
    * ci: cache config
    
    * ci: cache config
    
    * debug: ci conf
    
    * fix: config cache
    
    * fix: cache usage
    
    * chore: update triggers
    
    * debug: test time

--- client-mobile/jest.config.ts
+++ client-mobile/jest.config.ts
@@ -156,8 +156,8 @@ const config: Config = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-     "**/tests/**/*.[jt]s?(x)",
-   ],
+    "**/tests/**/*.[jt]s?(x)"
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
