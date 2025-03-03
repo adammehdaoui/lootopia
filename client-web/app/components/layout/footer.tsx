2585772b999f094086a0686af4110e60fac80b5e
LouisLagrange1 <ljblagrange@gmail.com>
2025-03-03 19:39:02 +0100

    add responsive homepage & layout

--- client-web/app/components/layout/footer.tsx
+++ client-web/app/components/layout/footer.tsx
@@ -4,21 +4,21 @@ import { IoLogoGooglePlaystore } from "react-icons/io5";
 
 export function Footer() {
   return (
-    <div className="bg-[#142247]  rounded-t-xl text-white p-16 ">
-      <div className="flex gap-96">
-        <span className="flex flex-col gap-1">
-          <p className="font-bold py-4">Reseaux sociaux</p>
+    <div className="bg-[#142247] rounded-t-xl text-white p-8 md:p-16">
+      <div className="flex flex-col md:flex-row gap-8 md:gap-24">
+        <span className="flex flex-col gap-4">
+          <p className="font-bold py-4">Réseaux sociaux</p>
           <a href="/" className="flex items-center gap-2 hover:scale-110">
             Facebook <Facebook size={20} />
           </a>
           <a href="/" className="flex items-center gap-2 hover:scale-110">
-            Instagram <Instagram size={20} />{" "}
+            Instagram <Instagram size={20} />
           </a>
           <a href="/" className="flex items-center gap-2 hover:scale-110">
             Twitter <Twitter size={20} />
           </a>
         </span>
-        <span className="flex flex-col gap-1">
+        <span className="flex flex-col gap-4">
           <p className="font-bold py-4">Download app</p>
           <a href="/" className="flex items-center gap-2 hover:scale-110">
             Apple Store <AiOutlineApple size={25} />
@@ -27,8 +27,8 @@ export function Footer() {
             Play Store <IoLogoGooglePlaystore size={20} />
           </a>
         </span>
-        <span className="flex flex-col gap-1">
-          <p className="font-bold py-4">Commment ca marche ?</p>
+        <span className="flex flex-col gap-4">
+          <p className="font-bold py-4">Comment ça marche ?</p>
           <a href="/" className="hover:underline">
             FAQ
           </a>
@@ -40,29 +40,25 @@ export function Footer() {
           </a>
         </span>
       </div>
-      <span className="flex items-center gap-6 pt-8">
-        <p className="font-semibold">
+      <span className="flex flex-col md:flex-row items-center gap-6 pt-8">
+        <p className="font-semibold text-center md:text-left">
           © 2025 Lootopia, Inc. Tous droits réservés
         </p>
-        <p>·</p>
-        <a href="/" className="hover:underline">
-          FAQ
-        </a>
-        <p>·</p>
+        <div className="flex gap-2 justify-center md:justify-start">
+          <a href="/" className="hover:underline">
+            Contact
+          </a>
+          <p>·</p>
 
-        <a href="/" className="hover:underline">
-          Contact
-        </a>
-        <p>·</p>
+          <a href="/" className="hover:underline">
+            Privacy Policy
+          </a>
+          <p>·</p>
 
-        <a href="/" className="hover:underline">
-          Privacy Policy
-        </a>
-        <p>·</p>
-
-        <a href="/" className="hover:underline">
-          Legal Notice
-        </a>
+          <a href="/" className="hover:underline">
+            Legal Notice
+          </a>
+        </div>
       </span>
     </div>
   );
