b65625db42c32c57cd402355a448c7aec2af002f
Adam Mehdaoui <adam.mehdaouijorge@gmail.com>
2025-03-07 16:10:51 +0100

    chore: format components

--- client-web/app/components/layout/navbar.tsx
+++ client-web/app/components/layout/navbar.tsx
@@ -1,16 +1,16 @@
-import DrawerMenu from "@/components/custom/drawer-menu";
-import Menu from "@/components/custom/menu";
-import RewardMenu from "@/components/custom/reward-menu";
+import DrawerMenu from "@/components/custom/drawer-menu"
+import Menu from "@/components/custom/menu"
+import RewardMenu from "@/components/custom/reward-menu"
 
 export default function Navbar() {
   return (
-    <div className="sticky rounded-b-xl top-0 z-10 flex justify-between bg-royal px-8 py-4">
+    <div className="sticky top-0 z-10 flex justify-between rounded-b-xl bg-royal px-8 py-4">
       <Menu />
 
       <div className="ml-5 mr-10 flex space-x-5">
         <RewardMenu />
         <DrawerMenu />
       </div>
-    </div >
+    </div>
   )
 }
