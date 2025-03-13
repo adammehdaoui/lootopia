773ac481d56a0bd27c3559f5cab1506366fca433
Adam Mehdaoui <adam.mehdaouijorge@gmail.com>
2025-03-13 19:12:00 +0100

    WIP: map config

--- client-mobile/app/map/index.tsx
+++ client-mobile/app/map/index.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
-
 import * as Location from "expo-location";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function () {
   const [location, setLocation] = useState<Location.LocationObject | null>(
@@ -26,5 +27,43 @@ export default function () {
     getCurrentLocation();
   }, []);
 
-  return <></>;
+  if (location === null) {
+    return (
+      <View>
+        <Text>Map is loading...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          }}
+          title="You are here"
+        />
+      </MapView>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
