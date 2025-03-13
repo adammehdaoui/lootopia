4b64c858471f3b3c03bd8d2e2bc7b3d81738758e
Adam Mehdaoui <adam.mehdaouijorge@gmail.com>
2025-03-13 18:42:36 +0100

    feat: get current location

--- client-mobile/app/map/index.tsx
+++ client-mobile/app/map/index.tsx
@@ -1,3 +1,30 @@
+import { useEffect, useState } from "react";
+
+import * as Location from "expo-location";
+
 export default function () {
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.error("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
+
   return <></>;
 }
