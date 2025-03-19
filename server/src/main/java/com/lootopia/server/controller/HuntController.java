2c2a9216077767a5399152dcbf9968d0bec5d1ef
Adam Mehdaoui <adam.mehdaouijorge@gmail.com>
2025-03-19 12:35:15 +0100

    feat: working popularity controller

--- server/src/main/java/com/lootopia/server/controller/HuntController.java
+++ server/src/main/java/com/lootopia/server/controller/HuntController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/hunt")
+@RequestMapping("/hunts")
 public class HuntController {
 
     private final HuntService huntService;
