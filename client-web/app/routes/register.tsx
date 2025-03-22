43aacbcc81064a846d1c5a4d6ee8e6eec0a40cb3
Adam Mehdaoui <adam.mehdaouijorge@gmail.com>
2025-03-22 11:33:13 +0100

    chore: register logic

--- client-web/app/routes/register.tsx
+++ client-web/app/routes/register.tsx
@@ -1,70 +1,9 @@
-import { Spinner } from "@/components/custom/spinner"
 import { Button } from "@/components/ui/button"
 import { useToast } from "@/hooks/use-toast"
 import { register } from "@/services/auth/auth"
 import { ActionFunctionArgs } from "@remix-run/node"
-import { Form, useActionData } from "@remix-run/react"
-import { useEffect, useState } from "react"
-
-export default function Signup() {
-  const actionData = useActionData<typeof action>()
-  const [loading, setLoading] = useState(false)
-  const { toast } = useToast()
-
-  useEffect(() => {
-    console.table(actionData)
-
-    if (actionData?.sent) {
-      setLoading(false)
-      return
-    }
-
-    if (actionData?.error) {
-      setLoading(false)
-      toast({
-        title: "Uh oh! Something went wrong.",
-        description: actionData.error,
-        variant: "destructive"
-      })
-    }
-  }, [actionData, actionData?.error, actionData?.sent, toast])
-
-  const handleSubmit = () => {
-    setLoading(true)
-
-    toast({
-      title: "An email has been sent",
-      description: "Check your inbox to confirm your email address"
-    })
-  }
-
-  return (
-    <div className="mx-auto my-32 flex max-w-lg flex-col items-center justify-center rounded-3xl border-4 border-white bg-royal p-8 sm:w-full sm:px-4">
-      <h1 className="pb-8 text-2xl font-bold text-white">Register</h1>
-      <Form
-        method="post"
-        className="flex w-full max-w-xs flex-col justify-center gap-4"
-        onSubmit={handleSubmit}
-      >
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          required
-          className="w-full rounded-md border-2 border-white bg-deep p-2 text-white"
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Mot de passe"
-          required
-          className="w-full rounded-md border-2 border-white bg-deep p-2 text-white"
-        />
-        <Button variant="submit">Submit {loading && <Spinner />}</Button>
-      </Form>
-    </div>
-  )
-}
+import { Form, useActionData, useNavigation } from "@remix-run/react"
+import { useEffect } from "react"
 
 export const action = async ({ request }: ActionFunctionArgs): Promise<ActionResponse> => {
   const formData = await request.formData()
@@ -82,6 +21,58 @@ export const action = async ({ request }: ActionFunctionArgs): Promise<ActionRes
   } catch (error: unknown) {
     return { error: error as string, sent: false }
   }
+}
+
+export default function Signup() {
+  const actionData = useActionData<typeof action>()
+  const navigation = useNavigation()
+  const { toast } = useToast()
+
+  useEffect(() => {
+    if (!(navigation.state === "idle" && actionData)) return
+
+    if (actionData.sent) {
+      toast({
+        title: "Success",
+        description: "An email has been sent, check your inbox to confirm your email address"
+      })
+
+      return
+    }
+
+    if (actionData.error) {
+      toast({
+        title: "Uh oh, something went wrong",
+        description: `${actionData.error}`,
+        variant: "destructive"
+      })
+
+      console.log("error")
+    }
+  }, [navigation.state, actionData, toast])
+
+  return (
+    <div className="mx-auto my-32 flex max-w-lg flex-col items-center justify-center rounded-3xl border-4 border-white bg-royal p-8 sm:w-full sm:px-4">
+      <h1 className="pb-8 text-2xl font-bold text-white">Register</h1>
+      <Form method="post" className="flex w-full max-w-xs flex-col justify-center gap-4">
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          className="w-full rounded-md border-2 border-white bg-deep p-2 text-white"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Mot de passe"
+          required
+          className="w-full rounded-md border-2 border-white bg-deep p-2 text-white"
+        />
+        <Button variant="submit">Submit</Button>
+      </Form>
+    </div>
+  )
 }
 
 type ActionResponse = { error?: string; sent: boolean }
