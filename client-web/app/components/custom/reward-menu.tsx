c668f70ffd9b603d1fce82f0153b593d9450a14a
Adam Mehdaoui <adam.mehdaouijorge@gmail.com>
2025-03-26 13:23:44 +0100

    chore: add avatar placeholder

--- client-web/app/components/custom/reward-menu.tsx
+++ client-web/app/components/custom/reward-menu.tsx
@@ -1,28 +1,32 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
 import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuList
 } from "@/components/ui/navigation-menu"
-import { Crown } from "lucide-react"
+import { AvatarFallback } from "@radix-ui/react-avatar"
 import { Link } from "@remix-run/react"
-import { FaUser } from "react-icons/fa"
+import { Crown } from "lucide-react"
 
 export default function RewardMenu() {
   return (
     <NavigationMenu>
-      <NavigationMenuList>
+      <NavigationMenuList className="flex space-x-5">
         <NavigationMenuItem>
           <Button variant="crown">
             <Crown size={30} />
             <span>999 | +</span>
           </Button>
         </NavigationMenuItem>
-        <Link to={"/login"} className="flex justify-end">
-          <FaUser className="ml-4 h-6 w-6 text-white duration-300 hover:text-slate-500" />
-        </Link>
-
-        <NavigationMenuItem></NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link to={"/login"} className="flex justify-end">
+            <Avatar>
+              <AvatarImage src="/public/assets/fallback.jpg" />
+              <AvatarFallback>LOO</AvatarFallback>
+            </Avatar>
+          </Link>
+        </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
   )
